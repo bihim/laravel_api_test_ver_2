d6939224200c1ca021c1b464f46c4909da047419
Zubaer Alam Bihim <zubaerbihim@gmail.com>
2021-03-06 00:20:45 +0600

    First commit

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::fallback(function(){
+    return response()->json([
+        'message'=>'Page not found. If error presists please contact with admin'
+    ], 404);
+});
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::post('/post', [PostController::class, 'store']);
+
+Route::get('/posts/{id}', [PostController::class, 'show']);
+
+Route::put('/posts/{id}', [PostController::class, 'update']);
+
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
